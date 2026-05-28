2c32e0e82c980371d071c29441df16a3b235c26d
leelei <liweicheng01@corp.netease.com>
2026-05-28 14:50:36 +0800

    feat: add accessible Slei UI foundations

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./components/Avatar";
+export * from "./components/Badge";
+export * from "./components/Button";
+export * from "./components/Dialog";
+export * from "./components/Input";
+export * from "./components/Tabs";
